eaa1fb0ec1142b59e163e31b5167e11c1b92e0ea
Lucas-OrbitaDigital <lucasorbitadigital@gmail.com>
2025-10-15 10:28:35 +0200

    common.php and .gitignore updated

--- src/common.php
+++ src/common.php
@@ -12,4 +12,5 @@ function jsonResponse($data, int $status = 200): void
     http_response_code($status);
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    exit;
 }
