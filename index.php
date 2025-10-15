afeb523fa2154fd6ba7f73328791a9887d9887c2
Lucas-OrbitaDigital <lucasorbitadigital@gmail.com>
2025-10-15 10:06:39 +0200

    inserted control of errors

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
 
 $config = require __DIR__ . '/config.php';
 require_once __DIR__ . '/vendor/autoload.php';
--- src/routes/productRoutes.php
+++ src/routes/productRoutes.php
@@ -4,6 +4,6 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($uri === '/products' && $method === 'GET') {
-    $controller->list();
+    $controller->getAllByName();
     exit;
 }
